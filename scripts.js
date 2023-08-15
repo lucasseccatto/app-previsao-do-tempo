6acad541c776c9faf4f497ecfa2dac35ec13c660
lucasseccatto <lucasseccatto1@gmail.com>
2023-08-15 13:36:26 -0300

    Refatorando o código javascript

--- scripts.js
+++ scripts.js
@@ -1,34 +1,44 @@
+const inputString = 'Gurinhém....';
+const resultado = removePontosEspeciais(inputString);
 
-
-
-let chave = "4a0824e92a4134d9835235ea8ec45436"
-
-function colocarNaTela(dados){
-    console.log(dados)
-
-    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
-    document.querySelector(".temp").innerHTML = Math.floor (dados.main.temp) + "ºC"
-    document.querySelector(".descricao").innerHTML = dados.weather [0].description
-    document.querySelector(".umidade").innerHTML = "Umidade " + dados.main.humidity + "%"
-    document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
-    
-} 
-
-async function buscarCidade(cidade){
-    let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" +
-    cidade + 
-    "&appid=" + 
-    chave + 
-    "&lang=pt_br" + 
-    "&units=metric"
-    )
-    .then(resposta => resposta.json())
-
-    colocarNaTela(dados)
+function removePontosEspeciais(str) {
+  const cleanStr = str.replace(/[^\w\s]/gi, '').replace(/\s+/g, '');
+  return cleanStr;
 }
 
-function cliqueiNoBotao(){
-    let cidade = document.querySelector(".input-cidade").value
-    
-    buscarCidade(cidade)
+let chave = '4a0824e92a4134d9835235ea8ec45436';
+
+function colocarNaTela(dados) {
+  document.querySelector('.cidade').innerHTML = 'Tempo em ' + dados.name;
+  document.querySelector('.temp').innerHTML =
+    Math.floor(dados.main.temp) + 'ºC';
+  document.querySelector('.descricao').innerHTML = dados.weather[0].description;
+  document.querySelector('.umidade').innerHTML =
+    'Umidade ' + dados.main.humidity + '%';
+  document.querySelector('.icone').src =
+    'https://openweathermap.org/img/wn/' + dados.weather[0].icon + '.png';
+}
+
+async function buscarCidade(cidade) {
+  let dados = await fetch(
+    'https://api.openweathermap.org/data/2.5/weather?q=' +
+      cidade +
+      '&appid=' +
+      chave +
+      '&lang=pt_br' +
+      '&units=metric'
+  ).then((resposta) => resposta.json());
+
+  colocarNaTela(dados);
+}
+
+function cliqueiNoBotao() {
+  let cidade = document.querySelector('.input-cidade').value;
+
+  if (cidade.trim() === '') {
+    alert('Por favor, insira o nome de uma cidade antes de pesquisar.');
+    return;
+  }
+
+  buscarCidade(cidade);
 }
